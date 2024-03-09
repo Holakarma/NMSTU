410c1cb6b73132f7c6b91bfe1c26660adccf6714
Holakarma <holakarmos@gmail.com>
2024-03-09 16:34:03 +0500

    added button component

--- src/shared/ui/Button/ui/Button.tsx
+++ src/shared/ui/Button/ui/Button.tsx
@@ -1,11 +1,40 @@
-import { classNames } from 'shared/aliases';
-import cls from './Button.module.scss';
+import { classNames } from "shared/aliases";
+import cls from "./Button.module.scss";
+import { ReactNode } from "react";
 
-interface ButtonProps {
-    className?:string
+export enum CategoryVariants {
+    ALL_CATEGORIES = "all-categories",
+    NEWS_CATEGORY = "news-category",
+    TEACHER_CATEGORY = "teacher-category",
+    HELP_CATEGORY = "help-category",
 }
-export const Button = ({ className }:ButtonProps) => (
-    <div>
-        <button className={classNames(cls.Button, {}, [className])}>Hello world</button>
-    </div>
-);
+
+export enum ButtonTheme {
+    PRIMARY = "primary",
+    OUTLINE = "outline",
+}
+
+interface ButtonProps extends React.HTMLProps<HTMLButtonElement> {
+    className?: string;
+    children?: ReactNode;
+    isSelected?: boolean;
+    category?: CategoryVariants;
+    theme?: ButtonTheme;
+}
+export const Button = (props: ButtonProps) => {
+    const { className, children, isSelected=false, category, theme=ButtonTheme.PRIMARY } =
+        props;
+    return (
+        <div>
+            <button
+                className={classNames(cls.Button, {}, [
+                    className,
+                    cls[theme],
+                    category ? cls[category] : "",
+                    isSelected ? cls["Button-selected"] : "",
+                ])}>
+                {children}  
+            </button>
+        </div>
+    );
+};
